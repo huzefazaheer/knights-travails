a38c5187afe4704da7050d76872322afef7ccd5d
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-03-29 16:14:50 +0500

    Inital commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Get current node
+// Get node you want to go to
+// Traveserce from node to any other node 
+// If move gets greater than 5 leave
+import { createLinkedList } from "./linkedlist";
+
+let arr = [[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0]]
+
+function knightsMoves(startPos, endPos){
+    
+}
---
+++ linkedlist.js
@@ -0,0 +1,129 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.header = null
+    }
+}
+
+export const createLinkedList = function(){
+
+    let basenode = new Node(null)
+    let listsize = 0
+
+    function append(val, node = basenode){
+        if(basenode.value == null){
+            basenode.value = val
+            listsize++
+        }else{
+            if (node.header == null){
+                node.header = new Node(val)
+                listsize++
+            }else{
+                append(val, node.header)
+            }
+        }
+    }
+
+    function prepend(val){
+        basenode1 = new Node(val)
+        basenode1.header = basenode
+        basenode = basenode1
+        listsize++
+    }
+
+    function at(index, node=basenode){
+        if(index != 0){
+            return at(index-1, node.header)
+        }else{
+            return node.value
+        }
+    }
+
+    function contains(value){
+        if (find(value) == null){
+            return false
+        }else {
+            return true
+        }
+    }
+
+    function find(value, node = basenode, index = 0){
+        if(node.value == value){
+            return index
+        }else {
+            if(node.header == null){
+                return null
+            }else{
+                return find(value, node.header, index = index+1)
+            }
+        }
+    }
+
+    function insertAt(index, value, node = basenode){
+        if (index == 0){
+            let tmpnode = basenode
+            basenode = new Node(value)
+            basenode.header = tmpnode
+        }else if(index == 1){
+            let tempnode = node
+            node.header = new Node(value)
+            node.header.header = tempnode.header
+        }else{
+            if(node.header!=null){
+                insertAt(index = index-1, value, node.header)
+            }else return null
+        }
+    }  
+
+    function removeAt(index, node=basenode){
+        if(index == 0){
+            basenode = node.header
+        }else if(index == 1){
+            node.header = node.header.header
+        }else if(node.header != null){
+            return removeAt(index-1, node.header)
+        }return null
+        
+    }
+
+    function head(){
+        return basenode.value
+    }
+
+    function tail(node=basenode){
+        if(node.header == null){
+            return node.value
+        }else{
+            return tail(node.header)
+        }
+    }
+
+    function pop(secondlastnode=basenode){
+        if(secondlastnode == basenode && basenode.header == null){
+            basenode.value = null
+        }else{
+            node = secondlastnode.header
+            if(node.header == null){
+                secondlastnode.header = null
+            }else{
+                return pop(secondlastnode.header)
+            }
+        }
+    }
+
+    function size(){
+        return listsize
+    }
+
+    function toString(node = basenode, result= ''){
+        result = result + '(' + node.value + ') ' + '-> '
+        if(node.header == null){
+            result = result + 'null'
+            return result
+        }else{
+            return toString(node.header, result)
+        }
+    }
+
+    return {toString, append, prepend, size, tail, head, pop, at, find, contains, insertAt, removeAt}
+}
